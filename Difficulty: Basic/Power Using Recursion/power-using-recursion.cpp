a80fbfeb2a41fe07233417399c4b41b89c5e89eb
Sandeep Sharma <175778127+sandeep-helps@users.noreply.github.com>
2026-09-21 22:54:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Power Using Recursion/power-using-recursion.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    int recursivePower(int n, int p) {
+        // code here
+        if(p == 0){
+            return 1;
+        }
+        return n * recursivePower(n, p-1);
+    }
+};
